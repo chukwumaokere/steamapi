57567637199e0bef6363d4c45a549a243d05d74e
chukwumaokere <chukwumaokere@yahoo.com>
2018-02-28 15:34:34 +0000

    fixed a lot of HTML image issues added an image not available image added border-radius added highlight on images added alt text added title text when you hover over the images added a background centered the footer (still a placeholder. i'll put a better footer in a bit)

---
+++ indexforapp.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<title>Pinots Steam API v1.0.0</title>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<style>
+</style>
+
+<div class="bgtwo">
+
+<div class="layer">
+
+<div> 
+<button class="close" aria-hidden="true" type="button" title="Close" style="margin-top: 0;margin-left: 10px;color: #ccc;" id="close">
+	<img src="close_x.png" width="22px">
+</button>
+</div>
+
+
+</div>
+	<div class="content">
+	<form action="results.php" method="POST" name="search">
+		<h1>Search for Steam Games</h1>
+		<input name="gamename" class="has-border has-padding full-width shadow" placeholder="Steam App ID or game name"></input>
+		<input class="half-width btn-shadow" type="submit" name="submit" value="Search" style="width:115px">
+	</form>
+	<p class="bottom-text" style="color: white">Type "List All Games" (case-sensitive) in search box for a full list of games </p>
+	</div>
+	
+</div>
+
+</div>
+<!-- Script for X button -->
+<script src="default.js"></script>
+
+<!--
+
+<?php
+/*
+if(isset($_POST['submit'])){
+	$url = "http://store.steampowered.com/api/appdetails?appids=";
+  //      echo "Now";
+        $gamename = $_REQUEST["gamename"];
+	//echo is_numeric($gamename) . "\n <br>";
+        //echo var_dump($gamename) . "\n <br>";
+        include_once 'db.php';
+        global $db;
+        if (is_numeric($gamename) == ''){
+                $q = "SELECT * FROM apps WHERE name LIKE \"%$gamename%\"";
+		if ($gamename == ''){
+			$q = '';
+                	echo "Please Specify a game name or app id from Steam";
+        	}
+		if ($gamename == 'List All Games'){
+			$q = "SELECT * FROM apps";
+		}
+	//	echo "$q \n <br>";
+        }else if (is_numeric($gamename) == 1) {
+                $q = "SELECT * FROM apps WHERE appid = $gamename";
+	//	echo "$q \n <br>";
+        }
+	else{
+		//Need to put 
+                echo "Please specify a game name or app id from Steam";
+        }
+
+        $res = $db->query($q);
+
+        while ($row =  $res->fetch_assoc() ){
+		$appid = $row['appid'];
+		$apiurl = $url . $appid;
+		$json = file_get_contents($apiurl);
+                echo "{$row['name']}\n <br>";
+		$jsonfixed = str_replace("\\", '', $json);
+		$jsonfixed = str_replace("<br>t", '<br>', $jsonfixed);
+		$jsonfixed = str_replace("<br />rn", "<br>", $jsonfixed);
+		//$jenc = json_encode($jsonfixed, true);
+		echo "<pre> $jsonfixed </pre>  <br>";
+        }
+
+}else{
+	//nothing was set.
+	die();
+}
+*/
+?>
+-->
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<title>Pinots Steam API v1.0.0</title>
+<head>
+<link rel="stylesheet" href="results.css">
+</head>
+<body class="bg">
+<div id="contents">
+<div id="container">
+<?php
+        $url = "http://store.steampowered.com/api/appdetails?appids=";
+  //      echo "Now";
+        $gamename = $_REQUEST["gamename"];
+        //echo is_numeric($gamename) . "\n <br>";
+        //echo var_dump($gamename) . "\n <br>";
+        include_once 'db.php';
+	if (isset($_GET['p'])){
+                $p = $_GET['p'];
+                $offset = ($p - 1) * 20;
+		$pageno = $p;
+	}
+        global $db;
+        if (is_numeric($gamename) == ''){
+		$qcount = "SELECT count(*) FROM apps WHERE name LIKE \"%$gamename%\"";
+			$resCount = $db->query($qcount);
+		while ($row =  $resCount->fetch_assoc() ){
+			$totalItems = $row['count(*)'];
+			
+		}
+		$totalPages = ceil($totalItems / 20);
+		if (isset($_GET['p'])){
+			$q = "SELECT * FROM apps WHERE name LIKE \"%$gamename%\" LIMIT 20 OFFSET $offset";
+		}else{
+			$q = "SELECT * FROM apps WHERE name LIKE \"%$gamename%\" LIMIT 20 OFFSET 0";
+			$pageno = 1;
+		}
+                if ($gamename == ''){
+                        $q = '';
+                        echo "Please Specify a game name or app id from Steam";
+                }
+                if ($gamename == 'List All Games'){
+                        $q = "SELECT * FROM apps";
+                }
+        }else if (is_numeric($gamename) == 1) {
+                $q = "SELECT * FROM apps WHERE appid = $gamename";
+		$pageno = 1;
+		$totalPages = 1;
+		$totalItems = 1;
+        }
+        else{
+                //Need to put 
+                echo "Please specify a game name or app id from Steam";
+        }
+
+        $res = $db->query($q);
+
+        while ($row =  $res->fetch_assoc() ){
+                $appid = $row['appid'];
+                $apiurl = $url . $appid;
+                $json = file_get_contents($apiurl);
+
+ //             echo "{$row['name']}\n <br>";
+
+		$data = json_decode($json, true);
+		$picture = $data[$appid]["data"]["header_image"];
+		if ($picture){
+			echo "<div class=\"griddy\"> <img class=\"gametile\" src=\"$picture\" alt=\"{$row['name']}\" title=\"{$row['name']}\"></img></div>";
+		}else{
+			 echo "<div class=\"griddy\"> <img class=\"gametile\" src=\"notavailable.jpg\" alt=\"{$row['name']}\" title=\"{$row['name']}\"></img></div>";
+		}
+        }
+	echo "</div>";
+	if ($totalItems > 20){
+		$nextpage = $pageno + 1;
+		$prevpage = $pageno - 1;
+		$lastp = $totalPages;
+		$firstp = 1;
+		if ($prevpage == 0){ 
+			$firstpage = "This is the";
+			$prevpageC = "First Page";
+		}elseif ($prevpage > 0){
+			$firstpage = "<a href=\"results.php?gamename=$gamename&p=$firstp\">First</a>";
+			$prevpageC = "<a href=\"results.php?gamename=$gamename&p=$prevpage\">Prev</a>";
+		}
+		if ($nextpage <= $totalPages){
+			$lastpage = "<a href=\"results.php?gamename=$gamename&p=$lastp\">Last</a>";
+			$nextpageC = "<a href=\"results.php?gamename=$gamename&p=$nextpage\">Next</a>";
+		}else{
+			$lastpage = "";
+			$nextpageC = "Last Page";
+		}
+		$pagination = "$firstpage $prevpageC - Page $pageno of $totalPages Page(s) for $totalItems Results - $nextpageC $lastpage";
+	}else{
+		$pagination = "Page $pageno of $totalPages Page(s) for $totalItems Result.";
+	}
+	echo "<div class=\"footer\">";
+        echo $pagination;
+	echo "</div>";
+
+?>
+</div>
+</body>
+</html>
