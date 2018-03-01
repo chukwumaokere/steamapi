e207cc01e7fe14e158147c372d0d6183427eeafb
chukwumaokere <chukwumaokere@yahoo.com>
2018-03-01 17:46:00 +0000

    adding style for hover and fixing grid placemetns. adding links to steam website

--- results.php
+++ results.php
@@ -2,11 +2,22 @@
 <title>Pinots Steam API v1.0.0</title>
 <head>
 <link rel="stylesheet" href="results.css">
+<!
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
+
 <body class="bg">
 <div id="contents">
 	<div id="centerme">
 	<div id="centeragain">
+	<h1 style="color:white;">Searching for "<?php echo $_REQUEST["gamename"]; ?>"...</h1>
 	<div id="container">
 <?php
         $url = "http://store.steampowered.com/api/appdetails?appids=";
@@ -40,7 +51,19 @@
                         echo "Please Specify a game name or app id from Steam";
                 }
                 if ($gamename == 'List All Games'){
-                        $q = "SELECT * FROM apps";
+//                        $q = "SELECT * FROM apps";
+			$qcount = "SELECT count(*) FROM apps";
+				$resCount = $db->query($qcount);
+				while ($row =  $resCount->fetch_assoc() ){
+		                        $totalItems = $row['count(*)'];
+                		}
+			$totalPages = ceil($totalItems / 20);
+			if (isset($_GET['p'])){
+        	                $q = "SELECT * FROM apps LIMIT 20 OFFSET $offset";
+                	}else{
+                        	$q = "SELECT * FROM apps LIMIT 20 OFFSET 0";
+	                        $pageno = 1;
+        	        }
                 }
         }else if (is_numeric($gamename) == 1) {
                 $q = "SELECT * FROM apps WHERE appid = $gamename";
@@ -49,7 +72,8 @@
 		$totalItems = 1;
         }
         else{
-                //Need to put 
+                //Need to put popup on main page.
+		//Need to put hover info show 
                 echo "Please specify a game name or app id from Steam";
         }
 
@@ -65,9 +89,9 @@
 		$data = json_decode($json, true);
 		$picture = $data[$appid]["data"]["header_image"];
 		if ($picture){
-			echo "<div class=\"griddy\"> <img class=\"gametile\" src=\"$picture\" alt=\"{$row['name']}\" title=\"{$row['name']}\"></img></div>";
+			echo "<div class=\"griddy\"> <a href=\"http://store.steampowered.com/app/$appid/\"><img class=\"gametile\" src=\"$picture\" alt=\"{$row['name']}\" title=\"{$row['name']}\"></img></a></div>";
 		}else{
-			 echo "<div class=\"griddy\"> <img class=\"gametile\" src=\"notavailable.jpg\" alt=\"{$row['name']}\" title=\"{$row['name']}\" style=\"max-height: 99.999%;\"></img></div>";
+			 echo "<div class=\"griddy\"> <a href=\"http://store.steampowered.com/app/$appid\"><img class=\"gametile not-avail\" src=\"./notavailableresz.png\" alt=\"{$row['name']}\" title=\"{$row['name']}\" ></img></a></div>";
 		}
         }
 	echo "</div>"; //container
